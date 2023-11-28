a5af001dccffe4630770f145266872ec3f2165b7
Cas <73138891+casiimir@users.noreply.github.com>
2023-11-28 12:26:28 +0100

    Python base - lezione 28 Novembre 2023

---
+++ lezioni-python/28-11-2023.py
@@ -0,0 +1,81 @@
+###### RICORDA DI TOGLIERE I COMMENTI ######
+
+##############################
+##### Operatori matematici   #
+##############################
+##
+##print(2 + 2) # Stampa 4
+##print(100 * 100)
+##
+##print("La somma di 2 e 5: ", (2 + 5))
+##print("La somma di 2 + 5: ", (2 + 5)) # Elementi vengono separati da una virgola
+### Addizione, Sottrazione, Moltiplicazione, Divisione, Modulo
+##
+##print("12 / 3 =>", (12 / 3)) # Divisione
+##print("12 % 3 =>", (12 % 3)) # Modulo
+##print("12 % 5 =>", (12 % 5)) # Modulo
+##
+##print("Potenza di 4 elevato alla 2 =>", 4**2) # Potenza
+
+####################
+### Valori e Tipi ##
+####################
+
+##print("ciao, Davide!")
+##print(100) ## Non possiamo vedere all'interpretazione se è str o int
+##print("100") ## Analogo
+##print(100, "=>", type(100)) ## Stampa il tipo definito
+##print("100 =>", type("100"))
+##print(1,000,000) ## Errore stampa 1 0 0
+##print(1.000.000) ## Errore il punto non puo essere usato
+##print("guarda quell'aquilone") ## Soluzione ai casi di apici interni alla stringa
+
+##############
+###Esercizi Modulo 1 ##
+##############
+## 1. Quanti secondi ci sono in 42 minuti e 42 secondi?
+## print("In 42 minuti e 42 secondi ci sono =>", (42 * 60) + 42, "secondi.")
+##########################################################################################
+## 2. Quante miglia ci sono in 10 chilometri? Suggerimento: un miglio equivale a 1,61 km
+## print("In 10 chilometri ci sono:", (1.61 * 10), "miglia")
+##########################################################################################
+
+
+##--------------------------------------------------------------------------------------------
+
+###############
+### Variabili##
+###############
+##nome = "Davide"
+##print(nome)  ## Stampa Davide
+##print("Il mio nome è:", nome) ## Stampa il valore della variabile
+##nome = "Casimiro" ## Riassegnazione della variabile nome
+##print("Il mio nome è:", nome) ## Stampa il nuovo valore della variabile
+##secondi_in_42minuti = (42 * 60) + 42
+##print(secondi_in_42minuti)
+#### Ti consiglio di nominare una variabile sempre con il trattino basso come separatore di parole, es: una_sola_parola
+
+
+
+#######################
+###Esercizi Modulo 2 ##
+#######################
+### Il prezzo di copertina di un libro è € 24,95, ma una libreria ottiene il 40% di sconto. I costi
+### di spedizione sono € 3 per la prima copia e 75 centesimi per ogni copia aggiuntiva. Qual è il
+### costo totale di 60 copie?
+#######################
+## prezzo_scontato = (24.95 * 60) / 100
+## costo_spedizione = (3 + (0.75 * 59))
+## print("Il costo totale di 60 libri per la libreria:", (60 * prezzo_scontato + costo_spedizione), "€.")
+
+
+
+
+
+
+
+
+
+
+
+
